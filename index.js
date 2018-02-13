57b5f69590ba0e50bd9c2868993de31e7be993ea
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-13 19:31:18 +0100

    Add start files

---
+++ index.js
@@ -0,0 +1,25 @@
+﻿process.stdin.setEncoding('utf-8');
+
+var OSinfo = require('./modules/Osinfo');
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instuction = input.toString().trim();
+		switch(instuction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/sayhello':
+				process.stdout.write('hello!\n');
+				break;	
+			case '/getOSinfo':
+				OSinfo.print();
+			break;		
+			default:
+			process.stderr.write('Wrong instuction!\n');
+		};	
+	}
+});
+
---
+++ modules/OSinfo.js
@@ -0,0 +1,27 @@
+﻿var os = require('os');
+var colors = require('colors');
+
+function getOSinfo() {
+	var type = os.type();	
+	if(type === 'Darwin') {
+		type = "OSX";
+	} else if (type === 'Windows NT') {
+		type = 'Windows';
+	}
+	var release = os.release();
+	var cpu = os.cpus()[0].model;
+	var uptime = os.uptime();
+	var userInfo = os.userInfo();
+
+	console.log('System: ', type);
+	console.log(colors.red('Release: %s'), release);
+	console.log('CPU model: ', cpu);
+	console.log('Uptime: ~ ', (uptime / 60).toFixed(0), 'min');
+	console.log('User name: ', userInfo.username);
+	console.log('Home dir: ', userInfo.homedir);
+
+	console.log('hello'.green);
+	
+}
+
+exports.print = getOSinfo;
